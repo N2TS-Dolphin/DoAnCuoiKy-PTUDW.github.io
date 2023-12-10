25bf0c311e563145cf91c8759106e795ecce251b
npt5767 <141230403+npt5767@users.noreply.github.com>
2023-12-10 21:33:42 +0700

    fix conflict

--- app.js
+++ app.js
@@ -47,6 +47,7 @@ var collectionRouter = require('./components/collection/index')
 var prodectDetailsRouter = require('./components/product_details/index')
 var searchRouter = require('./components/search/index');
 
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -65,8 +66,6 @@ app.use('/logout', logoutRouter);
 app.use('/collection', collectionRouter)
 app.use('/product_details', prodectDetailsRouter)
 app.use('/search', searchRouter);
-
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
