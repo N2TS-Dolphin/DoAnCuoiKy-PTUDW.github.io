a790ae9733763280f687be6cdb167dcca8db4801
npt5767 <141230403+npt5767@users.noreply.github.com>
2023-12-19 22:42:45 +0700

    filter price

--- components/collection/index.js
+++ components/collection/index.js
@@ -20,26 +20,46 @@ async function generateSort(type, allProducts) {
 }
   return allProducts;
 }
-async function generateData(category, page, sort = null, filter=null) {
-  // const currentCategory =
-  //     category.charAt(0).toUpperCase() + category.slice(1);
-  // const currentType = type
-  //     ? type.charAt(0).toUpperCase() + type.slice(1)
-  //     : null;
+async function generatePrice(price_min, price_max, allProducts) {
+
+  let minPrice=Number.MAX_SAFE_INTEGER;
+  let maxPrice=0;
+  for (const product of allProducts) {
+    const productPrice = product.price || 0;
+
+    if (productPrice < minPrice) {
+      minPrice = productPrice;
+    }
+
+    if (productPrice > maxPrice) {
+      maxPrice = productPrice;
+    }
+  }
+  if(!price_min){
+    price_min=minPrice;
+  }
+  if(!price_max){
+    price_max=maxPrice;
+  }
+
+  return allProducts.filter(product => {
+    const productPrice = product.price;
+    return productPrice >= price_min && productPrice <= price_max;
+  });
+}
+async function generateData(category, page, sort = null, manufacturer=null, price_min=null, price_max=null) {
+
   let allProduct = await Product
           .find()
           .lean()
           .exec();
-      // await productController.getProductByCategoryAndSubcategory(
-      //     category,
-      //     type
-      // );
+
   let count = await Product.countDocuments();
   if (sort) {
     allProduct = await generateSort(sort, allProduct);
   }
-  if (filter) {
-    allProduct = await generatePrice(filter, allProduct);
+  if (price_min || price_max) {
+    allProduct = await generatePrice(price_min, price_max, allProduct);
   } 
   const productData = allProduct.slice((page - 1) * 6, page * 6);
   const categories = [...new Set(allProduct.map(product => product.category))];
@@ -49,14 +69,14 @@ async function generateData(category, page, sort = null, filter=null) {
     products: productData,
     categories,
     manufacturers,
-      productCount: count,
+    productCount: count,
       pagination: {
         current: page,
         page,
         pageCount: Math.ceil(count / 6)
       },
-      curPage: page,
-      sortType: sort,
+    curPage: page,
+    sortType: sort,
   };
 }
 /* GET product page. */
@@ -64,11 +84,11 @@ router.get('/', async (req, res, next) => {
   let page = parseInt(req.query.page) || 1;
   const category = req.query.category;
   const sort = req.query.sort;
-  const filter = req.query.filter;
   const manufacturer = req.query.manufacturer;
-  console.log(category);
-  console.log(filter);
-  const data = await generateData(category, page, sort, filter);
+  const price_min = req.query.price_min;
+  const price_max = req.query.price_max;
+  
+  const data = await generateData(category, page, sort, manufacturer, price_min, price_max);
   res.render("collection/index", { ...data, user: req.user });
 });
 
