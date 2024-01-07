b106da2c7c885a5cd43ed39efb4c7e77dc2d3327
N2TS-Dolphin <sonsung2003@gmail.com>
2024-01-07 17:59:34 +0700

    Create forgotpassword system

--- app.js
+++ app.js
@@ -108,6 +108,7 @@ var homeARouter = require('./components/admin/home/index.js');
 var productARouter = require('./components/admin/product/index.js');
 var orderARouter = require('./components/admin/order/index.js');
 var cartRouter = require('./components/user/shoppingcart/index.js');
+var forgotpasswordRouter = require('./components/user/forgotpassword/index.js');
 
 // view engine setup
 app.engine("hbs", hbs.engine);
@@ -132,6 +133,7 @@ app.use('/home-admin', homeARouter);
 app.use('/product-admin', productARouter(upload));
 app.use('/order-admin', orderARouter);
 app.use('/shoppingcart', cartRouter);
+app.use('/forgotpassword', forgotpasswordRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
