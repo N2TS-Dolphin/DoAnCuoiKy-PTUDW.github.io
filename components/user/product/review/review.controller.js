5080f791561f666c04e2b05da796bca1e2cdf94b
dinhhuyhuy <tdhuy21@clc.fitus.edu.vn>
2024-01-07 16:01:36 +0700

    update user product review

--- components/user/product/review/review.controller.js
+++ components/user/product/review/review.controller.js
@@ -2,9 +2,8 @@ const reviewService = require("../review/review.service")
 const productService = require("../product.service")
 
 const addNewReview = async (req, res, next) => {
-    await reviewService.createNewReview(req.params.id, req.body.accountID, req.body.rating, req.body.content)
+    await reviewService.createNewReview(req.params.id, req.body.accountEmail, req.body.rating, req.body.content)
     const product = await reviewService.updateProductRating(req.params.id)
-    console.log(product)
     const reviews = await reviewService.getProductReview(req.params.id)
     res.json({reviews: reviews, product: product})
 }
